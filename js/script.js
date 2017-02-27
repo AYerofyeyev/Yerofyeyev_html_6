17db0625db2d3877a05a46772c5c441dcc34b6e8
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-02-27 15:23:22 +0200

    jcarousel is added

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function() {
+    $(".jcarousel").jcarousel({
+        list: ".jcarousel-list",
+        items: ".jcarousel-item",
+        animation: "slow",
+        transitions: true,
+        wrap: "circular",
+        vertical: false
+    });
+    $('.jcarousel-prev').jcarouselControl({ target: '-=1' });
+    $('.jcarousel-next').jcarouselControl({ target: '+=1' });
+});
