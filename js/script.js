00f62b69fe87a9354ff02c972d17879ddbc233b5
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-02-27 16:42:19 +0200

    dropdown menu is done

--- js/script.js
+++ js/script.js
@@ -7,6 +7,26 @@ $(function() {
         wrap: "circular",
         vertical: false
     });
-    $('.jcarousel-prev').jcarouselControl({ target: '-=1' });
+    $(".jcarousel-prev").jcarouselControl({ target: '-=1' });
     $('.jcarousel-next').jcarouselControl({ target: '+=1' });
+    $( '.dropdown' ).hover(
+      function(){
+        $(this).children('.submenu').slideDown(150);
+      },
+      function(){
+        $(this).children('.submenu').slideUp(150);
+      }
+    );
+    // $(".dropdown").mouseenter(
+    //   function () {
+    //     $(this).children.animate({
+    //       backgroundColor:"linear-gradient(to top, #0c3357, #25617b)",
+    //     }, 500 );
+    //   });
+    // $(".dropdown").mouseleave(
+    //   function() {
+    //     $(this).children.animate({
+    //       backgroundColor:"linear-gradient(to top, #000 0%, #2a2e32 85%, #576068)",
+    //     }, 500 );
+    //   });
 });
