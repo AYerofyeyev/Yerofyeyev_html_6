6f3c57e01022e480fb1b5dc66b4de6509be6b8ed
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-03-23 18:18:35 +0200

    template principle is done

--- js/script.js
+++ js/script.js
@@ -25,199 +25,203 @@ $(function() {
 
 //===============================================
 // Шаблон
-var content = document.getElementsByClassName("content");
-var goods =  {
+var goods =
+  {
     brandname: "Goodyear",
     model: "Ultra Grip Extreme",
     imagename: "img/11.png",
     price: "90,00 грн."
-  };
+  }
+;
 
+var article = $(".content--template").html();
 
-var tmpl = _.template(document.getElementById("content--template").innerHTML);
+var tmpl = _.template(article);
+
+var $content = tmpl(goods);
 
 console.log(tmpl);
 
-content.append(tmpl);
+$(".content").append($content);
 
 
 // Переменная для модуля
-  var options = [
-    {
-    "imgName": "img/brand--toyo.png",
-    "href": "#",
-    "imgAlt": "toyo",
-    "imgWidth": "83px",
-    "imgMargin": "5px 0 0 -1px"
-    },
-    {
-    "imgName": "img/brand--bridgestone.png",
-    "href": "#",
-    "imgAlt": "bridgestone",
-    "imgWidth": "97px",
-    "imgMargin": "0 0 0 0"
-    },
-    {
-    "imgName": "img/brand--continental.png",
-    "href": "#",
-    "imgAlt": "continental",
-    "imgWidth": "77px",
-    "imgMargin": "5px 0 0 1px"
-    },
-    {
-    "imgName": "img/brand--goofer.png",
-    "href": "#",
-    "imgAlt": "goofer",
-    "imgWidth": "59px",
-    "imgMargin": "4px 0 0 3px"
-    },
-    {
-    "imgName": "img/brand--avon.png",
-    "href": "#",
-    "imgAlt": "avon",
-    "imgWidth": "77px",
-    "imgMargin": "0 0 0 0"
-    },
-    {
-    "imgName": "img/brand--factory.png",
-    "href": "#",
-    "imgAlt": "factory",
-    "imgWidth": "57px",
-    "imgMargin": "1px 0 0 1px"
-    },
-    {
-    "imgName": "img/brand--pirelli.png",
-    "href": "#",
-    "imgAlt": "pirelli",
-    "imgWidth": "61px",
-    "imgMargin": "1px 0 0 1px"
-    },
-    {
-    "imgName": "img/brand--dunlop.png",
-    "href": "#",
-    "imgAlt": "dunlop",
-    "imgWidth": "72px",
-    "imgMargin": "0 0 0 3px"
-    }
-  ];
-
-  var $img = [];
-  var $listItem = [];
-  var $div = $(".jq--truescene");
-  var $ul = $(".jq--truebackstage");
-  var l = options.length;
-  var a = (Math.ceil(l / 2) - 1);
-  var w = +(options[a].imgWidth.slice(0, -2));
-  var arrowWidth = 0;
-
-  function trueMoveLeft () {
-    var $elem = $(".jq--truebrand");
-    var $clone = $elem[0].clone();
-    var width = +($elem[0].css("width").slice(0, -2));
-    var left = +($elem[0].css("margin-left").slice(0, -2));
-    left -= (width + 12);
-    $clone.appendTo($(".jq--truebackstage"));
-    arrowWidthCalc();
-    $elem[0].animate({marginLeft:  left}, 500);
-    $trueLink.animate({
-      width: (+$elem[a].imgWidth.slice(0, -2) + 24),
-      marginLeft: arrowWidth
-    }, 500);
-    setTimeout(function () {
-      $elem.remove();
-    }, 501);
-  };
-
-  function trueMoveRight() {
-    var $clone = $(".jq--truebrand:last").clone();
-    var width = +($(".jq--truebrand:last").css("width").slice(0, -2));
-    var left = +($(".jq--truebrand:last").css("margin-left").slice(0, -2));
-    var cloneLeft = -(width + 12);
-    $clone
-    .css({marginLeft: cloneLeft})
-    .insertBefore($(".jq--truebrand:first"));
-
-    var $elem = $(".jq--truebrand:first");
-    $elem.animate({marginLeft:  left}, 500);
-    setTimeout(function () {
-      $(".jq--truebrand:last").remove();
-    }, 501);
-  };
-
-  (function arrowWidthCalc() {
-  for (var i = 0; i < a; i++) {
-    var w = (+options[i].imgWidth.slice(0, -2) + 12);
-    console.log(w);
-    arrowWidth += w;
-  };
-  return arrowWidth;
-});
-
-// arrowWidthCalc();
-
-  for (var i = 0; i < options.length; i++) {
-    $listItem[i] = $("<li>")
-      .addClass("jq--truebrand")
-      .css({
-        display: "inline-block",
-        margin: options[i].imgMargin,
-        marginRight: "12px",
-        // float: "left"
-      });
-
-    $img[i] = $("<img>")
-    .attr({
-      src: options[i].imgName,
-      alt: options[i].imgAlt
-    })
-    .addClass("jq--trueImg")
-    .css({
-      width: options[i].imgWidth,
-      maxWidth: "97px"
-    });
-
-    $ul.append($listItem[i]);
-    $listItem[i].append($img[i]);
-  };
-
-  var $trueLink = $("<a>")
-  .addClass("jq--trueLink")
-  .attr({href: options[a].href})
-  .css({
-    display: "inline-block",
-    position: "absolute",
-    marginTop: "-52px",
-    marginLeft: arrowWidth  + 118,
-    border: "1px solid #213a4d",
-    height: "27px",
-    width: (+options[a].imgWidth.slice(0, -2) + 12)
-  })
-  .insertAfter($div);
-
-  $("<div>")
-    .addClass("jq--trueArrowLeft")
-    .css({
-      position: "absolute",
-      margin: "-52px 0 0 118px",
-      height: "27px",
-      width: arrowWidth,
-      cursor: "pointer"
-    })
-    .on("click", trueMoveLeft)
-    .insertAfter($div);
-
-  $("<div>")
-    .addClass("jq--trueArrowRight")
-    .css({
-      position: "absolute",
-      margin: "-52px 0 0 0",
-      marginLeft: (+$(".jq--truescene").css.width().slice(0, -2) - arrowWidth + 118 + $trueLink.width()),
-      height: "27px",
-      width: (+$(".jq--truescene").css.width().slice(0, -2) - arrowWidth),
-      cursor: "pointer"
-    })
-    .on("click", trueMoveRight)
-    .insertAfter($div);
+//   var options = [
+//     {
+//     "imgName": "img/brand--toyo.png",
+//     "href": "#",
+//     "imgAlt": "toyo",
+//     "imgWidth": "83px",
+//     "imgMargin": "5px 0 0 -1px"
+//     },
+//     {
+//     "imgName": "img/brand--bridgestone.png",
+//     "href": "#",
+//     "imgAlt": "bridgestone",
+//     "imgWidth": "97px",
+//     "imgMargin": "0 0 0 0"
+//     },
+//     {
+//     "imgName": "img/brand--continental.png",
+//     "href": "#",
+//     "imgAlt": "continental",
+//     "imgWidth": "77px",
+//     "imgMargin": "5px 0 0 1px"
+//     },
+//     {
+//     "imgName": "img/brand--goofer.png",
+//     "href": "#",
+//     "imgAlt": "goofer",
+//     "imgWidth": "59px",
+//     "imgMargin": "4px 0 0 3px"
+//     },
+//     {
+//     "imgName": "img/brand--avon.png",
+//     "href": "#",
+//     "imgAlt": "avon",
+//     "imgWidth": "77px",
+//     "imgMargin": "0 0 0 0"
+//     },
+//     {
+//     "imgName": "img/brand--factory.png",
+//     "href": "#",
+//     "imgAlt": "factory",
+//     "imgWidth": "57px",
+//     "imgMargin": "1px 0 0 1px"
+//     },
+//     {
+//     "imgName": "img/brand--pirelli.png",
+//     "href": "#",
+//     "imgAlt": "pirelli",
+//     "imgWidth": "61px",
+//     "imgMargin": "1px 0 0 1px"
+//     },
+//     {
+//     "imgName": "img/brand--dunlop.png",
+//     "href": "#",
+//     "imgAlt": "dunlop",
+//     "imgWidth": "72px",
+//     "imgMargin": "0 0 0 3px"
+//     }
+//   ];
+//
+//   var $img = [];
+//   var $listItem = [];
+//   var $div = $(".jq--truescene");
+//   var $ul = $(".jq--truebackstage");
+//   var l = options.length;
+//   var a = (Math.ceil(l / 2) - 1);
+//   var w = +(options[a].imgWidth.slice(0, -2));
+//   var arrowWidth = 0;
+//
+//   function trueMoveLeft () {
+//     var $elem = $(".jq--truebrand");
+//     var $clone = $elem[0].clone();
+//     var width = +($elem[0].css("width").slice(0, -2));
+//     var left = +($elem[0].css("margin-left").slice(0, -2));
+//     left -= (width + 12);
+//     $clone.appendTo($(".jq--truebackstage"));
+//     arrowWidthCalc();
+//     $elem[0].animate({marginLeft:  left}, 500);
+//     $trueLink.animate({
+//       width: (+$elem[a].imgWidth.slice(0, -2) + 24),
+//       marginLeft: arrowWidth
+//     }, 500);
+//     setTimeout(function () {
+//       $elem.remove();
+//     }, 501);
+//   };
+//
+//   function trueMoveRight() {
+//     var $clone = $(".jq--truebrand:last").clone();
+//     var width = +($(".jq--truebrand:last").css("width").slice(0, -2));
+//     var left = +($(".jq--truebrand:last").css("margin-left").slice(0, -2));
+//     var cloneLeft = -(width + 12);
+//     $clone
+//     .css({marginLeft: cloneLeft})
+//     .insertBefore($(".jq--truebrand:first"));
+//
+//     var $elem = $(".jq--truebrand:first");
+//     $elem.animate({marginLeft:  left}, 500);
+//     setTimeout(function () {
+//       $(".jq--truebrand:last").remove();
+//     }, 501);
+//   };
+//
+//   (function arrowWidthCalc() {
+//   for (var i = 0; i < a; i++) {
+//     var w = (+options[i].imgWidth.slice(0, -2) + 12);
+//     console.log(w);
+//     arrowWidth += w;
+//   };
+//   return arrowWidth;
+// });
+//
+// // arrowWidthCalc();
+//
+//   for (var i = 0; i < options.length; i++) {
+//     $listItem[i] = $("<li>")
+//       .addClass("jq--truebrand")
+//       .css({
+//         display: "inline-block",
+//         margin: options[i].imgMargin,
+//         marginRight: "12px",
+//         // float: "left"
+//       });
+//
+//     $img[i] = $("<img>")
+//     .attr({
+//       src: options[i].imgName,
+//       alt: options[i].imgAlt
+//     })
+//     .addClass("jq--trueImg")
+//     .css({
+//       width: options[i].imgWidth,
+//       maxWidth: "97px"
+//     });
+//
+//     $ul.append($listItem[i]);
+//     $listItem[i].append($img[i]);
+//   };
+//
+//   var $trueLink = $("<a>")
+//   .addClass("jq--trueLink")
+//   .attr({href: options[a].href})
+//   .css({
+//     display: "inline-block",
+//     position: "absolute",
+//     marginTop: "-52px",
+//     marginLeft: arrowWidth  + 118,
+//     border: "1px solid #213a4d",
+//     height: "27px",
+//     width: (+options[a].imgWidth.slice(0, -2) + 12)
+//   })
+//   .insertAfter($div);
+//
+//   $("<div>")
+//     .addClass("jq--trueArrowLeft")
+//     .css({
+//       position: "absolute",
+//       margin: "-52px 0 0 118px",
+//       height: "27px",
+//       width: arrowWidth,
+//       cursor: "pointer"
+//     })
+//     .on("click", trueMoveLeft)
+//     .insertAfter($div);
+//
+//   $("<div>")
+//     .addClass("jq--trueArrowRight")
+//     .css({
+//       position: "absolute",
+//       margin: "-52px 0 0 0",
+//       marginLeft: (+$(".jq--truescene").css.width().slice(0, -2) - arrowWidth + 118 + $trueLink.width()),
+//       height: "27px",
+//       width: (+$(".jq--truescene").css.width().slice(0, -2) - arrowWidth),
+//       cursor: "pointer"
+//     })
+//     .on("click", trueMoveRight)
+//     .insertAfter($div);
 
 
 });
